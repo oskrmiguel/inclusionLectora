a7be927f7347b3a7353278079b438262d9a9af97
edyfjm <edyjimerino1@gmail.com>
2024-02-18 10:28:22 -0500

    versio 2
    cambios en el audio

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -4,10 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Media root
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -117,6 +113,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+# Media root
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
